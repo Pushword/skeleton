00b9208c28579033dcaef20aa7e072be19eceb63
RobinDev <contact@piedweb.com>
2024-04-03 08:30:53 +0000

    prepare v1

--- bin/object-test.php
+++ bin/object-test.php
@@ -5,8 +5,9 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__.'/../../../vendor/autoload.php';
 
-(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+(new Dotenv())->bootEnv(__DIR__.'/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
+
 return $kernel->getContainer()->get('doctrine')->getManager();
--- public/index.php
+++ public/index.php
@@ -1,18 +1,16 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
-
+use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../../../vendor/autoload.php';
 
 (new Dotenv())->loadEnv(__DIR__.'/../.env');
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'dev';
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] ='1';
-
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = '1';
 
 umask(0000);
 
